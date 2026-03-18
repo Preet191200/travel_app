5b41b3c9433b09f93b0a60a706ee212c52c87fba
Preet191200 <pkachhia@rrc.ca>
2026-03-18 14:57:44 -0500

    Add Bootstrap styling to all pages

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,9 +1,20 @@
 class CountriesController < ApplicationController
   def index
+    @countries = Country.all
+
     if params[:search].present?
-      @countries = Country.where("name LIKE ? OR region LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
-    else
-      @countries = Country.all
+      @countries = @countries.where("name LIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:population].present?
+      case params[:population]
+      when "small"
+        @countries = @countries.where("population < ?", 10_000_000)
+      when "medium"
+        @countries = @countries.where("population >= ? AND population <= ?", 10_000_000, 50_000_000)
+      when "large"
+        @countries = @countries.where("population > ?", 50_000_000)
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "pages/about"
   root "countries#index"
+
+  get "about", to: "pages#about"
 
   resources :countries, only: [:index, :show] do
     resources :cities, only: [:index, :show]
